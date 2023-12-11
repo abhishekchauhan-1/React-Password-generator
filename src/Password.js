509215ddd3bbb86eaa5337ae81655e181681771c
Abhishek Singh chauhan <112499844+abhishekchauhan-1@users.noreply.github.com>
2023-12-11 17:38:25 +0530

    Update Password.js

--- src/Password.js
+++ src/Password.js
@@ -1,11 +1,15 @@
+// Import the CSS file for styling
 import "./style.css";
 
+// Import necessary modules and components from React and external libraries
 import { Component } from "react";
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCopy } from "@fortawesome/free-solid-svg-icons";
 
+// Define the Password component as a class component
 class Password extends Component {
+  // Constructor to initialize the component's state
   constructor() {
     super();
     this.state = {
@@ -18,6 +22,7 @@ class Password extends Component {
     };
   }
 
+  // Function to generate a password based on user preferences
   generatePassword = () => {
     const {
       sliderValue,
@@ -27,6 +32,7 @@ class Password extends Component {
       includeSymbols,
     } = this.state;
 
+    // Define character sets for different types of characters
     const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
     const symbolChars = "!@#$%^&*()_+[]{}|;:,.<>?";
@@ -35,6 +41,7 @@ class Password extends Component {
     let allowedChars = "";
     let password = "";
 
+    // Concatenate character sets based on user preferences
     if (isUpperCase) {
       allowedChars += uppercaseChars;
     }
@@ -48,31 +55,33 @@ class Password extends Component {
       allowedChars += symbolChars;
     }
 
-
-    for(let i=0;i<sliderValue;i++){
-        const randomValue = Math.floor(Math.random()*allowedChars.length);
-        password += allowedChars.charAt(randomValue);
+    // Generate the password based on the concatenated character set
+    for (let i = 0; i < sliderValue; i++) {
+      const randomValue = Math.floor(Math.random() * allowedChars.length);
+      password += allowedChars.charAt(randomValue);
     }
 
-    this.setState({password})
-
+    // Update the component state with the generated password
+    this.setState({ password });
   };
 
+  // Event handler for slider value change
   handleSliderChange = (event) => {
     const newvalue = event.target.value;
     this.setState({ sliderValue: newvalue });
   };
 
-  copyToClipBoard = ()=>{
-    const {password} = this.state;
-    navigator.clipboard.writeText(password).then(()=>{
-        alert("Copied");
-    }).catch((error)=>{
-        console.error("failed to Copy",error);
-    })
-  }
+  // Function to copy the generated password to the clipboard
+  copyToClipBoard = () => {
+    const { password } = this.state;
+    navigator.clipboard.writeText(password).then(() => {
+      alert("Copied");
+    }).catch((error) => {
+      console.error("Failed to Copy", error);
+    });
+  };
 
-
+  // Render method for rendering the component
   render() {
     const {
       isUpperCase,
@@ -82,21 +91,27 @@ class Password extends Component {
       password
     } = this.state;
 
+    // JSX code for rendering the Password component
     return (
       <div className="main-container">
         <p>Password Generator</p>
         <div className="textArea">
-        <textarea className="textElement" defaultValue={password} placeholder="P4$5W0rD!" readOnly></textarea>
-        <button className="icon-btn" onClick={this.copyToClipBoard}>
-          <FontAwesomeIcon className="icon-btn" icon={faCopy} />
-        </button>
+          {/* Display the generated password in a textarea */}
+          <textarea className="textElement" defaultValue={password} placeholder="P4$5W0rD!" readOnly></textarea>
+          {/* Button to copy the password to the clipboard */}
+          <button className="icon-btn" onClick={this.copyToClipBoard}>
+            <FontAwesomeIcon className="icon-btn" icon={faCopy} />
+          </button>
         </div>
-        
+
+        {/* Section for user preferences and password generation */}
         <div className="elements">
           <div className="charlength">
+            {/* Display the character length and slider value */}
             <p className="charP">Character Length</p>
             <p className="zeroP">{this.state.sliderValue}</p>
           </div>
+          {/* Slider for selecting the character length */}
           <input
             type="range"
             id="slider"
@@ -109,7 +124,9 @@ class Password extends Component {
             onChange={this.handleSliderChange}
           />
 
+          {/* Checkbox options for including different types of characters */}
           <div className="checkbox">
+            {/* Uppercase letters */}
             <input
               type="checkbox"
               id="upperCase"
@@ -121,6 +138,7 @@ class Password extends Component {
               Include Uppercase Letter
             </label>
             <br />
+            {/* Lowercase letters */}
             <input
               type="checkbox"
               id="lowerCase"
@@ -132,33 +150,33 @@ class Password extends Component {
               Include Lowercase Letter
             </label>
             <br />
+            {/* Numbers */}
             <input
               type="checkbox"
               id="Numbers"
               name="option"
               className="checkbox1"
-              onChange={() =>
-                this.setState({ includeNumbers: !includeNumbers })
-              }
+              onChange={() => this.setState({ includeNumbers: !includeNumbers })}
             />
             <label htmlFor="Numbers" className="includelabel">
               Include Numbers
             </label>
             <br />
+            {/* Symbols */}
             <input
               type="checkbox"
               id="symbols"
               name="option"
               className="checkbox1"
-              checked = {includeSymbols}
-              onChange={() =>
-                this.setState({ includeSymbols: !includeSymbols })
-              }
+              checked={includeSymbols}
+              onChange={() => this.setState({ includeSymbols: !includeSymbols })}
             />
             <label htmlFor="symbols" className="includelabel">
               Include Symbols
             </label>
           </div>
+
+          {/* Display the password strength */}
           <div className="strength">
             <p>STRENGTH</p>
             <div className="inside-div">
@@ -168,6 +186,8 @@ class Password extends Component {
               <div></div>
             </div>
           </div>
+
+          {/* Button to generate a new password */}
           <button className="btn" onClick={this.generatePassword}>GENERATE</button>
         </div>
       </div>
@@ -175,4 +195,5 @@ class Password extends Component {
   }
 }
 
+// Export the Password component as the default export of this module
 export default Password;
